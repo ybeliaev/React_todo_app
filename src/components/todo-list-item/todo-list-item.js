5f367d41a944aaa6c08002b75f0dd91181a208ff
Yura <ybeliaev@gmail.com>
2019-01-21 12:37:26 +0200

    add class
    вместо функций используем классы

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,33 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import './todo-list-item.css';
+import "./todo-list-item.css";
 
-const TodoListItem = ({ label, important = false }) => {
+class TodoListItem extends React.Component {
+  render() {
+    const { label, important = false } = this.props;
+    const style = {
+      color: important ? "steelblue" : "black",
+      fontWeight: important ? "bold" : "normal"
+    };
 
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  };
+    return (
+      <span className="todo-list-item">
+        <span className="todo-list-item-label" style={style}>
+          {label}
+        </span>
 
-  return (
-    <span className="todo-list-item">
-      <span
-        className="todo-list-item-label"
-        style={style}>
-        {label}
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+        >
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+        >
+          <i className="fa fa-trash-o" />
+        </button>
       </span>
-
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
-};
+    );
+  }
+}
 
 export default TodoListItem;
