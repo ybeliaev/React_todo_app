38a33bb52edce33abcb52a2079f3434946b2d569
Yura <ybeliaev@gmail.com>
2019-01-21 16:00:27 +0200

    add state ' done '

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,16 +3,34 @@ import React, { Component } from "react";
 import "./todo-list-item.css";
 
 export default class TodoListItem extends Component {
+  state = {
+    done: false
+  };
+  onLabelClick = () => {
+    this.setState({
+      done: true
+    });
+  };
+
   render() {
     const { label, important = false } = this.props;
+    const { done } = this.state;
+    let classNames = "todo-list-item";
+    if (done) {
+      classNames += " done";
+    }
     const style = {
       color: important ? "steelblue" : "black",
       fontWeight: important ? "bold" : "normal"
     };
 
     return (
-      <span className="todo-list-item">
-        <span className="todo-list-item-label" style={style}>
+      <span className={classNames}>
+        <span
+          className="todo-list-item-label"
+          style={style}
+          onClick={this.onLabelClick}
+        >
           {label}
         </span>
 
