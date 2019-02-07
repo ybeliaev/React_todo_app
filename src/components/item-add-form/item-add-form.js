36f8c69f96487656a17bfe9fc02d3ab6fffc01ac
Yura <ybeliaev@gmail.com>
2019-02-07 16:16:35 +0200

    add ' HelloWorld ' for test event of added

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,9 +4,16 @@ import "./item-add-form.css";
 
 export default class ItemAddForm extends Component {
   render() {
+    const { onAddItem } = this.props;
     return (
       <div className="item-add-form">
-        <button className="btn btn-warning">Add item</button>
+        <button
+          className="btn btn-warning"
+          onClick={() => this.props.onAddItem("Hello World")}
+        >
+          {" "}
+          Add item
+        </button>
       </div>
     );
   }
