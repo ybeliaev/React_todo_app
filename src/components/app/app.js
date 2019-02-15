91880cf5064fa089290ff360c138c4eab09da0be
Yura <ybeliaev@gmail.com>
2019-02-15 20:59:47 +0200

    doneCount

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -70,9 +70,10 @@ export default class App extends Component {
   };
 
   render() {
+    const doneCount = this.state.todoData.filter(el => el.done);
     return (
       <div className="todo-app">
-        <AppHeader toDo={1} done={3} />
+        <AppHeader toDo={1} done={doneCount} />
         <div className="top-panel d-flex">
           <SearchPanel />
           <ItemStatusFilter />
