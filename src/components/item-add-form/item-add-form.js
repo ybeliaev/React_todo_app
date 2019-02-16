0a8f2fbf9ff35d99606323d7bfe675b935976d1d
Yura <ybeliaev@gmail.com>
2019-02-16 13:53:03 +0200

    onLabelChange

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,22 +3,29 @@ import React, { Component } from "react";
 import "./item-add-form.css";
 
 export default class ItemAddForm extends Component {
+  state: {
+    label: ""
+  };
+  onLabelChange = e => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.onAddItem(this.state.label);
+    this.state.label = "";
+  };
   render() {
-    const { onAddItem } = this.props;
     return (
-      <form className="item-add-form">
+      <form className="item-add-form" onSubmit={this.onSubmit}>
         <input
           type="text"
           className="form-control"
           onChange={this.onLabelChange}
           placeholder="Whats need to be done?"
         />
-        <button
-          className="btn btn-warning"
-          onClick={() => onAddItem("Hello World")}
-        >
-          Add item
-        </button>
+        <button className="btn btn-warning">Add item</button>
       </form>
     );
   }
